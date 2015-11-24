52c5f4ddfde8db6179f11c3bec2bc8be69eed238
Ian Cordasco <graffatcolmingov@gmail.com>
2015-11-24 12:23:51 -0600

    Handle stdin in the plugin
    Closes #2

--- flake8_docstrings.py
+++ flake8_docstrings.py
@@ -2,6 +2,9 @@
 """pep257 docstrings convention needs error code and class parser for be
 included as module into flake8
 """
+import io
+
+import pep8
 import pep257
 
 __version__ = '0.2.1.post1'
@@ -14,13 +17,25 @@ class pep257Checker(object):
     name = 'pep257'
     version = __version__
 
+    STDIN_NAMES = set(['stdin', '-', '(none)', None])
+
     def __init__(self, tree, filename='(none)', builtins=None):
         self.tree = tree
         self.filename = filename
+        self.source = self.load_source()
+        self.checker = pep257.PEP257Checker()
 
     def run(self):
         """Use directly check() api from pep257."""
-        for error in pep257.check([self.filename]):
+        for error in self.checker.check_source(self.source, self.filename):
             # Ignore AllError, Environment error.
             if isinstance(error, pep257.Error):
                 yield (error.line, 0, error.message, type(self))
+
+    def load_source(self):
+        if self.filename in self.STDIN_NAMES:
+            self.filename = 'stdin'
+            self.source = pep8.stdin_get_value()
+        else:
+            with io.open(self.filename, encoding='utf-8') as fd:
+                self.source = fd.read()
