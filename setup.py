42e3efb2678cec1db62b9315de6fa0c278bd93e1
Chris Abiad <gitlab@chrisabiad.com>
2019-07-09 15:06:44 +0000

    Set max pydocstyle version to < 4.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
             'D = flake8_docstrings:pep257Checker',
         ],
     },
-    install_requires=['flake8', 'pydocstyle >= 2.1', 'flake8-polyfill'],
+    install_requires=['flake8', 'pydocstyle >= 2.1, < 4.0', 'flake8-polyfill'],
     provides=['flake8_docstrings'],
     py_modules=['flake8_docstrings'],
 )
